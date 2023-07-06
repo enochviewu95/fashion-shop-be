99a2fae4bedd2312c8f5f9eeaddd282b4de4d4b5
enochviewu95 <viewuenoch@gmail.com>
2023-07-06 18:46:38 +0000

    Added allowed origins to node code

--- app.js
+++ app.js
@@ -17,18 +17,28 @@ const authRouter = require("./routes/auth");
 const passport = require("passport");
 const mongoUri = process.env.MONGODB_URI;
 
+
+const allowedOrigins = [process.env.ORIGIN, process.env.DOMAIN]
+
+const corsOptions = {
+  origin: function(origin, callback){
+    if(allowedOrigins.indexOf(origin) !== -1 || !origin){
+      callback(null, true)
+    } else{
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true
+}
+
 const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.use(
-  cors({
-    origin: process.env.ORIGIN,
-    credentials: true,
-  })
-);
+app.use(cors(corsOptions));
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
