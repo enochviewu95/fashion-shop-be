66c663a30946af0638a6867f03e25c262a08652d
enochviewu95 <viewuenoch@gmail.com>
2023-07-07 08:12:07 +0000

    Made some changes

--- middlewares/is-auth.js
+++ middlewares/is-auth.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
   if (!req.isAuthenticated()) {
-    res.redirect(process.env.FASHION_DOMAIN_NAME_VALUE + "auth");
+    res.redirect(process.env.FASHION_DOMAIN_NAME_VALUE + "/auth");
   } else {
     if (req.user.role !== "admin") {
       res.redirect(process.env.FASHION_DOMAIN_NAME_VALUE);
